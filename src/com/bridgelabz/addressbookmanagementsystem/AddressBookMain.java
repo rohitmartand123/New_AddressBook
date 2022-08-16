e37a05aace4e8b4376c0219ca5f3f62659d03407
rohitmartand123 <rohitmartand274@gmail.com>
2022-08-16 20:14:02 +0530

    Added:Ability to create contact in Address Book

---
+++ src/com/bridgelabz/addressbookmanagementsystem/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbookmanagementsystem;
+/**
+ * Ability to create a Contacts in Address Book with first and last names, address, city, state, zip,
+ * phone number and email
+ *
+ * @author : Rohit
+ */
+public class AddressBookMain {
+	 public static void main(String[] args) {
+
+	        System.out.println("------------------Welcome to Address Book Management System !---------------------");
+
+	    }
+}
